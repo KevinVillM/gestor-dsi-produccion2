b336bc6baf5bb21af3c677f78fde947d59a2337e
KevinVillM <89365161+KevinVillM@users.noreply.github.com>
2023-09-15 21:35:11 -0600

    Se edito el ejemplo de carga de archivo
    Se agrego la animacion de carga de archivo

--- public/js/animate.js
+++ public/js/animate.js
@@ -2,9 +2,20 @@ console.log('Animate.js loaded');
 
 const myForm = document.getElementById('myForm');
 const imput = document.getElementById('file-upload');
+const bandera = document.getElementById('bandera');
+const cargaIcon = document.getElementById('cargaIcon');
 
 myForm.addEventListener('submit', async(e) => {
   e.preventDefault();
+
+
+  bandera.innerHTML = "Subiendo archivo...";
+
+  //Hacer visible el div cargaIcon
+  cargaIcon.style.visibility = "visible";
+  
+
+
   const formData = new FormData();
   console.log("/////////////"+imput);
   formData.append('archivo', imput.files[0]);
@@ -16,12 +27,13 @@ var requestOptions = {
   body: formData,
   redirect: 'follow'
 };
-
+// Agregar un una pleca que gire mientras se sube el archivo
 await fetch("http://localhost:8080/api/uploads/64500a36140b2e6f85fd87d0", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
-
+  cargaIcon.style.visibility = "hidden";
+  bandera.innerHTML = "Archivo subido con exito";
 });
 
 
